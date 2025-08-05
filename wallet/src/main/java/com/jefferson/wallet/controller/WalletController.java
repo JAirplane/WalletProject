b0740156c59e06686400c8575c588de5d4ab6018
eugen <eugeneshevchenko0@gmail.com>
2025-08-05 21:08:46 +0500

    controller url issue fixed

--- wallet/src/main/java/com/jefferson/wallet/controller/WalletController.java
+++ wallet/src/main/java/com/jefferson/wallet/controller/WalletController.java
@@ -4,6 +4,7 @@ import com.jefferson.wallet.dto.BalanceDto;
 import com.jefferson.wallet.dto.TransactionRequest;
 import com.jefferson.wallet.dto.WalletDto;
 import com.jefferson.wallet.service.PaymentService;
+import com.jefferson.wallet.service.PaymentServiceImpl;
 import com.jefferson.wallet.service.WalletService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +18,7 @@ import java.util.UUID;
 
 @Slf4j
 @RestController
-@RequestMapping(path = "/api/v1/", produces = "application/json")
+@RequestMapping(path = "/api/v1", produces = "application/json")
 public class WalletController implements WalletApi {
 
     private final WalletService walletService;
@@ -39,7 +40,7 @@ public class WalletController implements WalletApi {
     }
 
     @Override
-    @GetMapping("wallet/{walletId}")
+    @GetMapping("/wallet/{walletId}")
     public ResponseEntity<BalanceDto> getWalletBalance(@PathVariable UUID walletId) {
         log.info("Processing get wallet balance request. Wallet id: {}.Timestamp: {}", walletId, Instant.now());
 
@@ -48,7 +49,7 @@ public class WalletController implements WalletApi {
     }
 
     @Override
-    @DeleteMapping("wallet/{walletId}")
+    @DeleteMapping("/wallet/{walletId}")
     public ResponseEntity<Void> deleteWallet(@PathVariable UUID walletId) {
         log.info("Processing delete wallet request. Wallet id: {}. Timestamp: {}", walletId, Instant.now());
 
@@ -58,7 +59,7 @@ public class WalletController implements WalletApi {
     }
 
     @Override
-    @PostMapping("wallet")
+    @PostMapping("/wallet")
     public ResponseEntity<BalanceDto> processWalletOperation(@RequestBody @Valid TransactionRequest transactionRequest) {
         log.info("Processing wallet operation balance request. Wallet id: {}. Operation type: {}. Amount: {}. Timestamp: {}",
                 transactionRequest.walletId(), transactionRequest.operationType(), transactionRequest.amount(), Instant.now());
@@ -67,4 +68,5 @@ public class WalletController implements WalletApi {
 
         return ResponseEntity.ok(balanceDto);
     }
+
 }
